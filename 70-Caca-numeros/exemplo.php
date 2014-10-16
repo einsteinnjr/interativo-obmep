ead7f65ab1686d7002738d98d051bb1bb1f4d8a7
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-10-16 10:27:58 -0300

    +added sideSelection, mouseOver table and generate a table full of primes.

--- 70-Caca-numeros/exemplo.php
+++ 70-Caca-numeros/exemplo.php
@@ -8,20 +8,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	<strong> Selecione os números abaixo: </strong>
+	<h4>Caça-números</h4>
+	<div>
+		<strong> Ache os números primos no tabuleiro: </strong>
+	</div>
+</br>
+	<div>
+		<strong> Qual o tamanho do tabuleiro? </strong>
+		<div>
+			<label class="radio-inline">
+			  <input type="radio" name="tableSize" value="small"  onclick="decideSideLength(this)" checked> pequeno
+			</label>
+			<label class="radio-inline">
+			  <input type="radio" name="tableSize" value="large" onclick="decideSideLength(this)" > grande
+			</label>
+		</div>
+	</div>
+</br>
+	<button type="button" onClick="generateNewTable();">Novo Tabuleiro</button>
 	<div class="table-responsive">	
 		<table id="gameTable" class="table table-striped table-bordered" ></table>			
-		<div> 
-		</div>
 	</div>
 
 
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
-	<script src="src/js/teorema70.js"></script>
+        <script type="text/javascript" src="src/js/teorema70.js"></script>	
 	<script>
 		$(document).ready(function () {
 			init();
-			$("#gameTable").html(generateNewTableHtml());
 		});
 	</script>
 </body>
--- 70-Caca-numeros/src/js/teorema70.js
+++ 70-Caca-numeros/src/js/teorema70.js
@@ -3,15 +3,19 @@
 //168 primes less than 1000
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
 
+var others = [];
 
-function generateNewTableHtml(){
-	var n = 5;
+var sideLength=5;
+var MAX_NUMBER=1000;
+
+function generateNewTableHtml(n){
 	var tableHtml="";
-	var idx = 0;
+	var idx=0;
 	for(var i=0; i<n; i++){
-		tableHtml+="<tr>"
+		tableHtml+="<tr>";
 		for(var j=0; j<n; j++){
-			tableHtml+="<td>"+primes[idx]+"</td>";
+			//every tableElement must be a random number
+			tableHtml+="<td id=\"elem_"+idx+"\" > "+primes[Math.floor(Math.random()*primes.length)]+"</td>";
 			idx++;
 		}
 		tableHtml+="</tr></br>"
@@ -19,10 +23,52 @@ function generateNewTableHtml(){
 	return tableHtml;
 }
 
+function toggleIt(idx){
+	console.log('elem_'+idx);
+	$('#elem_'+idx).toggleClass("mouse-over");
+}
+
+function generateOthers(){
+	var i=0, j=1;
+	while(i<primes.length){
+		if(j<primes[i]){ //se j não estiver no array primes, adicione em others
+			others.push(j);
+		}
+		else{//se j tiver chegado no valor do primes[i], podemos ir ao proximo valor.
+			i++;		
+		}
+		j++;//ande ao proximo numero.
+	}
+	while(j<=MAX_NUMBER){// preencha others até o MAX_NUMBER
+		others.push(j);		
+		j++;
+	}
+	for(i=0;i<others.length; i++){
+		console.log(others[i]);
+	}
+}
+
+function decideSideLength(myRadio){
+	if(myRadio.value==="small") sideLength=5;
+	else if(myRadio.value==="large") sideLength=7;
+}
+
+function generateNewTable(){
+	$("#gameTable").html(generateNewTableHtml(sideLength));
+	mouseEventsOnTable();
+}
+
+function mouseEventsOnTable(){
+	$("table td").mouseenter(function(){
+		$(this).addClass("mouse-over");
+	});
+	$("table td").mouseout(function(){
+		$(this).removeClass("mouse-over");
+	});	
+}
+
 function init(){
-
-
+	generateNewTable();
+	generateOthers();
 };
 
-
-//$powers = {"2":[]}
