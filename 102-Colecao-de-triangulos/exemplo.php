16604e2d515b614710928aeb883e79e990020998
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-10-19 12:28:18 -0300

    *structure exemplo.php files, and changes filenames to teorema102.

--- 102-Colecao-de-triangulos/exemplo.php
+++ 102-Colecao-de-triangulos/exemplo.php
@@ -2,55 +2,57 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="utf-8" />
-	<title>101 - Caça-primos</title>
+	<title>102 - Coleção de triângulos</title>
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" type="text/css" href="src/css/teorema101.css">
+	<link rel="stylesheet" type="text/css" href="src/css/teorema102.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	<h4><strong>Caça-Primos:</strong></h4>
+	<h4><strong>Coleção de triângulos:</strong></h4>
 	<div>
-		<strong> Ache todos os números primos no tabuleiro abaixo. </strong>
+		<p><strong> Renato, desde criança, sempre gostou de geometria. E, por isso, faz coleção de armações triangulares. Seu tio, sabendo da paixão do sobrinho, permitiu que ele fosse a seu galpão antigo e recolhesse varetas de metais de forma a construir mais triângulos para sua coleção. </strong></p>
+		<p><strong> Devido a ser muito organizado, Renato, guarda sua coleção em grupos, dependendo do tipo de triângulo: equiláteros, isósceles, acutângulo, etc. Ajude-o a classificar os grupos de 3 varetas, dentro de sua coleção, ou informe que não é possivel fazer uma armação triangular com o grupo. </strong></p>
 	</div>
 	<div>
-		<strong>- Escolha o nivel desejado: </strong>
+		<div class="checkbox">
+		        <label>
+		            <input type="checkbox" name="cant_construct_a_triangle" value="c" /> Não é possível construir triângulo.
+		        </label>
+            	</div>
+	</div>
+	<div>
+		<p><strong> Em caso de triângulo:</strong> </p>		
 		<div>
+			<strong> - Classifique-o quanto ao maior ângulo: </strong></br>
 			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="easy"  onclick="decideGameLevel(this)" checked> Fácil
+			  <input type="radio" name="greaterAngleClass" value="acutangle"  onclick="decideSideLength(this)"> Acutângulo
 			</label>
 			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="hard" onclick="decideGameLevel(this)" > Difícil
+			  <input type="radio" name="greaterAngleClass" value="rectangle" onclick="decideSideLength(this)" > Retângulo
+			</label>
+			<label class="radio-inline">
+			  <input type="radio" name="greaterAngleClass" value="obtusangle" onclick="decideSideLength(this)" > Obtusângulo
 			</label>
 		</div>
-	</div>
-	<div>
-		<strong>- Escolha o tamanho do tabuleiro: </strong>
 		<div>
+		<strong> - Classifique-o quanto aos lados: </strong></br>
 			<label class="radio-inline">
-			  <input type="radio" name="tableSize" value="small"  onclick="decideSideLength(this)" checked> Pequeno
+			  <input type="radio" name="sidesClass" value="scalene"  onclick="decideSideLength(this)" > Escaleno
 			</label>
 			<label class="radio-inline">
-			  <input type="radio" name="tableSize" value="medium" onclick="decideSideLength(this)" > Médio
+			  <input type="radio" name="sidesClass" value="isosceles" onclick="decideSideLength(this)" > Isósceles
 			</label>
 			<label class="radio-inline">
-			  <input type="radio" name="tableSize" value="large" onclick="decideSideLength(this)" > Grande
+			  <input type="radio" name="sidesClass" value="equilatero" onclick="decideSideLength(this)" > Equilátero
 			</label>
 		</div>
 	</div>
 </br>
-	<button type="button" class="btn btn-default" onClick="generateNewTable();">Novo Tabuleiro</button>
-	<div class="table-responsive">	
-		<table id="gameTable" class="table table-striped table-bordered" ></table>			
-	</div>
-	<!--<div>	
-		<strong>Números de primos achados: []</strong>
-</br>
-		<button type="button" onClick="generateNewTable();">Revelar Dica</button>		
-		<strong>Total de primos é: </strong> <input disabled></input>			
-	</div>-->
+	<button type="button" class="btn btn-default" onClick="generateNewTable();">Revelar Resposta</button>
+	
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
-        <script type="text/javascript" src="src/js/teorema101.js"></script>	
+        <script type="text/javascript" src="src/js/teorema102.js"></script>	
 	<script>
 		$(document).ready(function () {
 			init();
