b0a1c8950258c2a508a8c775a04d3cb971b16105
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-10-20 15:10:09 -0300

    +adding library for geometry and graphs in javascript

--- 102-Colecao-de-triangulos/exemplo.php
+++ 102-Colecao-de-triangulos/exemplo.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<title>102 - Coleção de triângulos</title>
+	<link rel="stylesheet" type="text/css" href="assets/jsxgraph/css/jsxgraph.css">
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet" type="text/css" href="src/css/teorema102.css">
@@ -12,17 +13,23 @@
 	<h4><strong>Coleção de triângulos:</strong></h4>
 	<div>
 		<p><strong> Renato, desde criança, sempre gostou de geometria. E, por isso, faz coleção de armações triangulares. Seu tio, sabendo da paixão do sobrinho, permitiu que ele fosse a seu galpão antigo e recolhesse varetas de metais de forma a construir mais triângulos para sua coleção. </strong></p>
-		<p><strong> Devido a ser muito organizado, Renato, guarda sua coleção em grupos, dependendo do tipo de triângulo: equiláteros, isósceles, acutângulo, etc. Ajude-o a classificar os grupos de 3 varetas, dentro de sua coleção, ou informe que não é possivel fazer uma armação triangular com o grupo. </strong></p>
+		<p><strong> Devido a ser muito organizado, Renato guarda sua coleção em grupos, dependendo do tipo de triângulo: equilátero, isósceles, acutângulo, etc. Ajude-o a classificar os grupos de 3 varetas, dentro de sua coleção ou informe que não é possivel fazer uma armação triangular com o grupo. </strong></p>
 	</div>
+		<div>
+		<table>
+		</div>	
+	<div>
+		<div id="questionBox" class="jxgbox"></div>
+	</div>	
 	<div>
 		<div class="checkbox">
 		        <label>
-		            <input type="checkbox" name="cant_construct_a_triangle" value="c" /> Não é possível construir triângulo.
+		            <input type="checkbox" name="cant_construct_a_triangle" value="c" /> Não é possível construir o triângulo.
 		        </label>
             	</div>
 	</div>
 	<div>
-		<p><strong> Em caso de triângulo:</strong> </p>		
+		<p><strong> Se for possível a construção do triângulo:</strong> </p>		
 		<div>
 			<strong> - Classifique-o quanto ao maior ângulo: </strong></br>
 			<label class="radio-inline">
@@ -49,13 +56,16 @@
 		</div>
 	</div>
 </br>
-	<button type="button" class="btn btn-default" onClick="generateNewTable();">Revelar Resposta</button>
-	
+	<button type="button" class="btn btn-default" onClick="revealAnswer()	;">Revelar Resposta</button>
+
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
-        <script type="text/javascript" src="src/js/teorema102.js"></script>	
+	<script src="assets/jsxgraph/js/jsxgraphcore.js"></script>
+        <script type="text/javascript" src="src/js/teorema102.js"></script>
+		
+	<div id="answerBox"></div>
 	<script>
 		$(document).ready(function () {
-			init();
+
 		});
 	</script>
 </body>
