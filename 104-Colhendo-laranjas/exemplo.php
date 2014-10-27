0eda5c1cedf01c7cb3b5e5814aa839196b9481ba
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-10-27 07:05:34 -0300

    *[104] initial text for exemplo.php. Renamed former to 104.js and 104.css

--- 104-Colhendo-laranjas/exemplo.php
+++ 104-Colhendo-laranjas/exemplo.php
@@ -2,50 +2,28 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="utf-8" />
-	<title>103 - Pense em um número</title>
-	<link rel="stylesheet" type="text/css" href="assets/jsxgraph/css/jsxgraph.css">
+	<title>104 - Colhendo laranjas</title>
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" type="text/css" href="src/css/teorema103.css">
+	<link rel="stylesheet" type="text/css" href="src/css/teorema104.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
 <body>
-	<h4><strong>Pense em um número:</strong></h4>
+	<h4><strong>Colhendo laranjas:</strong></h4>
 	<div>
-		<p><strong> Bruno e Bernardo gostavam de jogar um jogo de adivinhação. Bruno pedia que Bernardo pensasse em um número de 1 a 100, por exemplo. Bruno, então, tentaria adivinhar o número após uma série de perguntas a Bernardo com respostas sim ou não. (exemplo: O número que você pensou é menor ou igual a 17?).  </strong></p>
-		<p><strong> Depois de algum tempo jogando, eles descobriram que havia um número mínimo de perguntas que se poderia fazer para que sempre fosse possível acertar o número de Bernardo. E você? Consegue descobrir o número pensado por Bernardo fazendo somente um número pequeno de perguntas? </strong></p>
+		<p><strong> Bernardo costuma colher laranjas em um pomar de formato triangular. Ele é repleto de laranjeiras dispostas em linhas horizontais, de forma que cada linha tem uma laranjeira a menos que a linha debaixo dela. Além disso, a laranjeira é dita vizinha de suas 2 vizinhas imediatamente abaixo delas (diagonal esquerda e diagonal direita). Os números representam a quantidade de laranjas em cada uma delas. </strong></p>
+		<p><strong>Bernardo deseja descobrir qual o caminho deve fazer, partindo da laranjeira mais alta e seguindo apenas para laranjeiras vizinhas para baixo, de forma a conseguir colher o maior número de laranjas, chegando a última linha. 
+		<p><strong>Clique nas laranjeiras de forma a definir o caminho. Assim que tiver um caminho completo (com uma laranjeira por linha), você pode testar se achou o caminho com maior número de laranjas. </strong></p>
 	</div>
-	<div class="column-left">
-			<div>
-			<button type="button" class="btn btn-default" onClick="generateNewGame();">Novo Jogo</button> 
-			</div>
-			<table id="requisites" class="table table-bordered">
-			<tr>
-				<td><b>Intervalo de números:</b></td>
-				<td id="interval"></td>
-			</tr>
-			<tr>
-				<td><b>Número de Perguntas:</b></td>
-				<td id="numberOfQuestions"></td>
-			</tr>
-		</table>	
-	
-	</div>	
-	<div class="column-right">
-		<table id="questions" class="table table-striped table-bordered">
-		</table>
-	</div>
-	<div>
-	  <button id="revealExplanation" type="button" class="btn btn-default btn-primary" onClick="revealExplanation();">Revelar Explicação</button> 
-	  <div id="secret" class="bordered">
-	  </div>
-	</div>
-	
+	<div class="orchard">
 
+	</div>	
+	
+	
+	<p>fonte do problema: <a href="https://projecteuler.net/problem=18">Project Euler</a></p>
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
-	<script src="assets/jsxgraph/js/jsxgraphcore.js"></script>
-        <script type="text/javascript" src="src/js/teorema103.js"></script>
+        <script type="text/javascript" src="src/js/teorema104.js"></script>
 	
 	<script>
 		$(document).ready(function () {
