e0c025891c5c093257f4684b874b3d5f9b352bba
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-10-27 02:00:01 -0300

    *Finished for fixed Max number 100. ToDo: Fix layout.

---
+++ 103-Pense-em-um-numero/src/js/teorema103.js
@@ -0,0 +1,130 @@
+var MAX_NUMBER = 100;
+var NUMBER_OF_QUESTIONS= 7;
+
+var thinkedNumber;
+var actualQuestion = 1;
+var numberAsked;
+var guessedNumber;
+
+function thinkNumber(){
+	thinkedNumber=Math.floor(Math.random()*MAX_NUMBER);
+	//alert("thinked "+thinkedNumber);
+};
+
+function isValidNumberAsked(){
+	numberAsked = document.getElementById("number_asked_"+actualQuestion).value;
+	//take out leading and trailing spaces.	
+	numberAsked=numberAsked.trim();
+	if(numberAsked.length===0 ||isNaN(numberAsked)) return false;
+	else return true;
+};
+
+function sendQuestion(){	
+	if(isValidNumberAsked()){
+		$("#answer_"+actualQuestion).empty();
+		answerQuestion();
+		actualQuestion++;
+		if(actualQuestion<=NUMBER_OF_QUESTIONS) generateQuestionLine();
+		else generateGuessLine(); //ended all questions.
+	}
+	else {
+		alert("O valor digitado "+numberAsked+" não é um número válido. Favor, digite um número.");
+		document.getElementById("number_asked_"+actualQuestion).value="";	
+	}
+};
+
+function answerQuestion(){
+	var x = document.getElementById("select_"+actualQuestion).selectedIndex;
+	numberAsked = document.getElementById("number_asked_"+actualQuestion).value;
+	switch(document.getElementsByTagName("option")[x].value){
+		case "greater_than":
+			if(thinkedNumber > numberAsked) $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-ok'>Verdade!</i>");
+			else $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-remove'>Falso!</i>");
+	break;
+		case "greater_or_equal":
+			if(thinkedNumber >= numberAsked) $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-ok'>Verdade!</i>");
+			else $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-remove'>Falso!</i>");
+	break;
+		case "less_than":
+			if(thinkedNumber < numberAsked) $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-ok'>Verdade!</i>");
+			else $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-remove'>Falso!</i>");
+	break;
+		case "less_or_equal":
+			if(thinkedNumber <= numberAsked) $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-ok'>Verdade!</i>");
+			else $("#answer_"+actualQuestion).html("<i class='glyphicon glyphicon-remove'>Falso!</i>");
+	break;
+	}
+	document.getElementById("number_asked_"+actualQuestion).disabled=true;
+	document.getElementById("select_"+actualQuestion).disabled=true;
+};
+
+function generateInterval(){
+	$("#interval").empty();
+	$("#interval").html("1 a "+MAX_NUMBER);
+};
+
+function generateNumberOfQuestions(){
+	$("#numberOfQuestions").empty();
+	$("#numberOfQuestions").html(NUMBER_OF_QUESTIONS);
+};
+
+function resetTable(){
+	$("#questions").empty();
+	$("#questions").append("<tr id='thead'>"+
+			     "<td>Pergunta (n<sup>o</sup>)</td>"+
+			     "<td>Tipo de pergunta</td>"+
+			     "<td>Valor</td>"+
+			     "<td>Resposta</td>"+
+			       "</tr>");
+	$("#guess").remove();
+};
+
+function generateQuestionLine(){
+	$("#questions").append(" <tr>"+
+			"<td id='"+actualQuestion+"'>"+actualQuestion+"<sup>a</sup></td>"+
+			   "<td><select id='select_"+actualQuestion+"'>"+
+			  "<option value='greater_than'>É maior que </option>"+
+			  "<option value='greater_or_equal'>É maior ou igual a </option>"+
+			  "<option value='less_than'>É menor que </option>"+
+			  "<option value='less_or_equal'>É menor ou igual a </option>"+
+					"</select> </td>"+
+			   "<td><input id='number_asked_"+actualQuestion+"' required placeholder='Escolha um número'></input>?</td>"+
+			"<td id='answer_"+actualQuestion+"'><button type='button' class='btn btn-default' onClick='sendQuestion();'>Enviar Pergunta</button></td>"+
+			"</tr>");	
+};
+
+function generateGuessLine(){
+	$("#questions").after("<div id='guess'>"+
+				"<p>As perguntas esgotaram. Qual o número que Bernardo pensou?</p>"+
+				"<input id='guessedNumber' required></input>"+
+				"<button type='button' class='btn btn-default' onClick='guessNumber();'>Adivinhar número</button>"+
+				"</div>");
+	
+};
+
+function guessNumber(){
+	var playAgain;
+	guessedNumber=document.getElementById("guessedNumber").value;
+	console.log("guessedNumber "+guessedNumber);
+	console.log("thinkedNumber "+thinkedNumber);
+	if(parseInt(guessedNumber) === parseInt(thinkedNumber)) {
+		playAgain = confirm("Parabéns! Você acertou o número de Bernardo! É "+thinkedNumber+". Deseja jogar novamente?");
+	}
+	else {
+		playAgain = confirm("Que Pena! Você errou. O número pensado por Bernardo foi "+thinkedNumber+". Deseja jogar novamente?"); 
+	}
+	if(playAgain === true){
+		generateNewGame(); 
+	}
+}
+
+function generateNewGame(){
+	actualQuestion=1;
+	thinkNumber();
+	generateInterval();
+	generateNumberOfQuestions();
+	resetTable();
+	generateQuestionLine();
+};
+
+
