4b42b69c06ed7622acdae32ea7241c2386a7a77d
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-11-05 11:58:15 -0300

    [105] rename files to correct ones and links on example.php

--- 105-Ordenando-fracoes/exemplo.php
+++ 105-Ordenando-fracoes/exemplo.php
@@ -2,20 +2,19 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="utf-8" />
-	<title>104 - Colhendo laranjas</title>
+	<title>105 - Ordenando frações</title>
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" type="text/css" href="src/css/teorema104.css">
+	<link rel="stylesheet" type="text/css" href="src/css/teorema105.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
 <body>
-	<h4><strong>Colhendo laranjas:</strong></h4>
+	<h4><strong>Ordenando frações:</strong></h4>
 	<div>
-		<p><strong> Bernardo costuma colher laranjas em um pomar de formato triangular, como o abaixo. Os números representam a quantidade de frutos em cada uma delas. </strong></p>		
-		<p><strong>Deseja-se descobrir qual o caminho deve fazer partindo da laranjeira mais alta e chegando até a última linha, apenas seguindo direções inferior esquerda ou inferior direita e de maneira a colher o maior número de laranjas possível. 
-		<p><strong>Clique nas árvores de forma a definir o caminho. Assim que tiver um caminho completo, com uma laranjeira por linha, você pode testar se achou a solução. Utilize os botões abaixo. </strong></p>
+		<p><strong> Ordene as frações abaixo da menor para a maior: </strong></p>		
 	</div>
+	<div id="orchard"></div>
 	<div class="spaced">
 		<strong>- Escolha o nivel desejado: </strong>
 		<div>
@@ -29,18 +28,13 @@
 			  <input type="radio" name="gameLevel" value="hard"> Difícil
 			</label>
 		</div>
-	</div>
-	<button id="newGame" type="button" class="btn btn-default" onClick="generateNewGame();">Novo Jogo</button>
-	<div id="orchard"></div>	
-	
-	<div class="btn-group">
-		<button id="testPath" type="button" class="btn btn-default" onClick="validateIfItIsMaxPath();">Testar Caminho</button>
-		<button id="testPath" type="button" class="btn btn-default btn-primary" onClick="showMaxPath();">Mostrar Caminho Máximo</button>	
 	</div>	
-
-	<p class="spaced">fonte do problema: <a href="https://projecteuler.net/problem=18">Project Euler</a></p>
+	<div class="btn-group">
+		<button id="testPath" type="button" class="btn btn-default" onClick="validateIfItIsMaxPath();">Testar Ordenação</button>
+		<button id="testPath" type="button" class="btn btn-default btn-primary" onClick="showMaxPath();">Nova Lista</button>	
+	</div>	
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
-        <script type="text/javascript" src="src/js/teorema104.js"></script>
+        <script type="text/javascript" src="src/js/teorema105.js"></script>
 	
 	<script>
 		$(document).ready(function () {
