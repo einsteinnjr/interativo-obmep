081dea2282ee403a72bb55038feb9efc079b7522
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-11-23 05:43:28 -0300

    [106] +1st mvp version. already moving plates according with weights.

--- 106-Balanca-de-pesos-especiais/index.php
+++ 106-Balanca-de-pesos-especiais/index.php
@@ -11,26 +11,24 @@
 	
 </head>
 <body>	
-	<div class="spaced">
-		<strong>- Escolha o nivel desejado: </strong>
-		<div>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="easy" checked> Fácil
-			</label>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="medium"> Médio
-			</label>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="hard"> Difícil
-			</label>
+	<div>
+	<ul id="ws" class="weights connectedSortable">
+		<li id='x' class="inline weight blue" value="0"><b>X</b></li>
+		<li id='w1' class="inline weight lgray" value="1">1kg</li>
+		<li id='w3' class="inline weight lgray" value="3">3kg</li>
+		<li id='w9' class="inline weight lgray" value="9">9kg</li>
+		<li id='w27' class="inline weight lgray" value="27">27kg</li>
+	<ul>
+	</div>	
+	<div id="balance" class="inline">
+		<div class="inline side"> 
+			<ul id="p1" class="plate inline connectedSortable"></ul>
+		</div>	
+		<div id='c' class="inline column"></div>
+		<div class="inline side">
+			<ul id="p2" class="plate inline connectedSortable"></ul>
 		</div>
 	</div>	
-	<div class="spaced">
-		<button id="newFractionList" type="button" class="btn btn-default btn-primary" onClick="generateNewGame();">Nova Lista</button>	
-	</div>	
-	<div>
-	<ul id="fractionsList" class="horizontalList spaced"></ul>
-	</div>
 	<div class="spaced">
 		<button id="testSorting" type="button" class="btn btn-default" onClick="validateSorting();">Testar Ordenação</button>
 	</div>	
@@ -40,6 +38,14 @@
         <script type="text/javascript" src="src/js/teorema106.js"></script>
 
 	<script>
+
+	$(function() {
+		$( "#ws, #p1, #p2" ).sortable({
+			connectWith: ".connectedSortable",
+			update: function(){weightPlates();}
+		}).disableSelection();
+	});
+	
 	$(document).ready(function () {
 		generateNewGame();
 	});
