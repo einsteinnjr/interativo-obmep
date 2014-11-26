1b5292db4e6024aa1b08da933cb5ac85f3064f5b
Einstein do Nascimento <einsteinnjr@gmail.com>
2014-11-26 16:33:35 -0300

    [105] *add bordered rectangles to delimit areas on interactive

--- 105-Ordenando-fracoes/index.php
+++ 105-Ordenando-fracoes/index.php
@@ -11,30 +11,34 @@
 	
 </head>
 <body>	
-	<div class="spaced">
-		<strong>- Escolha o nivel desejado: </strong>
-		<div>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="easy" checked> Fácil
-			</label>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="medium"> Médio
-			</label>
-			<label class="radio-inline">
-			  <input type="radio" name="gameLevel" value="hard"> Difícil
-			</label>
+	<div class="bordered spaced">
+		<div class="spaced">
+			<strong>- Escolha o nivel desejado: </strong>
+			<div>
+				<label class="radio-inline">
+				  <input type="radio" name="gameLevel" value="easy" checked> Fácil
+				</label>
+				<label class="radio-inline">
+				  <input type="radio" name="gameLevel" value="medium"> Médio
+				</label>
+				<label class="radio-inline">
+				  <input type="radio" name="gameLevel" value="hard"> Difícil
+				</label>
+			</div>
+		</div>	
+		<div class="spaced">
+			<button id="newFractionList" type="button" class="btn btn-default btn-primary" onClick="generateNewGame();">Nova Lista</button>	
 		</div>
-	</div>	
-	<div class="spaced">
-		<button id="newFractionList" type="button" class="btn btn-default btn-primary" onClick="generateNewGame();">Nova Lista</button>	
-	</div>	
-	<div class="spaced">
-	<ul id="fractionsList" class="horizontalList spaced"></ul>
 	</div>
-	<div class="spaced">
-		<button id="testSorting" type="button" class="btn btn-default btn-success" onClick="validateSorting();">Testar Ordenação</button>
-	</div>	
-	<div id="solution" class="spaced hidden"> </div>
+	<div class="bordered spaced	">	
+		<div class="spaced">
+		<ul id="fractionsList" class="horizontalList spaced"></ul>
+		</div>
+		<div class="spaced">
+			<button id="testSorting" type="button" class="btn btn-default btn-success" onClick="validateSorting();">Testar Ordenação</button>
+		</div>	
+	</div>
+	<div id="solution" class="spaced hidden bordered"> </div>
 
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
 	<script src="assets/jquery-ui/js/jquery-ui.min.js"></script>
