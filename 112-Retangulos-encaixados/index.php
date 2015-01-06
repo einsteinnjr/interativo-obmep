8f27f8487a51047824397f1d0b29171763e1f8ef
Einstein do Nascimento <einsteinnjr@gmail.com>
2015-01-05 21:56:06 -0300

    [112] *new problem. [Retangulos-encaixados]. +Added initial dummy files from [110]

---
+++ 112-Retangulos-encaixados/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="assets/jsxgraph/css/jsxgraph.css">
+        <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+        <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+	<link rel="stylesheet" type="text/css" href="src/css/teorema110.css">
+</head>
+<body>
+<div class="spaced-v">
+  <button type="button" class="btn btn-default  btn-primary" onClick="generateNewGame()">Novos Retângulos</button>
+</div>
+<div class="spaced-v bordered">
+	<div id="questionJXGBox" class="jxgbox center-img"></div>
+</div>	
+<div><b>Fonte do Problema:</b> <a href="http://clubes.obmep.org.br/blog/probleminha-divisao-justa/">Clubes Obmep - Problema Divisão Justa</a>
+</div>
+
+<div class="spaced-v">
+  <button id="showAnswer" type="button" class="btn btn-default btn-success spaced-h" onClick="showAnswer();">Mostrar Explicação</button> 
+</div> 
+
+	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
+	<script src="assets/jsxgraph/js/jsxgraphcore.js"></script>
+        <script type="text/javascript" src="src/js/teorema110.js"></script>
+	<script type="text/javascript" src="assets/MathJax/MathJax.js"></script>
+	
+	<div>	
+		<div id="answerExplanation" class="bordered answer-bg hidden height-auto">
+		</div>
+	</div>
+	<script>
+	$(document).ready(function () {
+		generateNewGame();
+	});
+	MathJax.Hub.Config({
+	  config: ["MMLorHTML.js"],
+	  jax: ["input/AsciiMath","output/HTML-CSS","output/NativeMML"],
+	  extensions: ["asciimath2jax.js","MathMenu.js","MathZoom.js"],
+	  imageFont: null
+	});
+	</script>
+</body>
+</html>
