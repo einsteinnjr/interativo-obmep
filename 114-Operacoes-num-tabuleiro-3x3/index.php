14d64d57d96f3cd7a4822e96e1f396a741b7fbf1
Einstein do Nascimento <einsteinnjr@gmail.com>
2015-01-20 18:17:19 -0300

    [114] finalized.

--- 114-Operacoes-num-tabuleiro-3x3/index.php
+++ 114-Operacoes-num-tabuleiro-3x3/index.php
@@ -3,22 +3,50 @@
 <head>
 	<meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/jquery-ui/css/jquery-ui.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet" type="text/css" href="src/css/interactive114.css">
 </head>
 <body>
 <div class="spaced-v">
-  <button type="button" class="btn btn-default  btn-primary" onClick="generateNewGame()">Novo ponto</button>
+  <button type="button" class="btn btn-default  btn-primary" onClick="generateNewGame()">Nova configuração</button>
 </div>
+
+
 <div class="spaced-v bordered">
-	<div id="questionJXGBox" class="jxgbox center-img"></div>
+	<div class="spaced-v">
+		<b>Operação:</b> <select id='numberToAdd' class="form-control selectwidthauto inline"><option value='1'>Somar +1</option><option value='-1'>Somar -1</option></select>
+	</div>	
+	<div class="spaced-v spaced-h inline bordered">
+		<div>
+			<div class="spaced-v"><b>Tabuleiro interativo:</b></div>
+			<table id="gameTable" ></table>
+		</div>
+		<div id="dGameSum" class="hidden">
+			<div id="lGameSum"><b>Soma atual do tabuleiro: </b><div class="gameSumValue green inline"></div></div>
+			<div id="l3-remainderGameSum"><b>Resto por 3 da soma atual: </b><div class="3-remainderGameSumValue green inline"></div></div>
+		</div>
+	</div>
+	<div class="spaced-v inline bordered">
+		<div>
+		<div class="spaced-v"><b>Estado final desejado:</b></div>
+		<table id="desiredFinalTable" ></table>
+		</div>
+		<div id="dFinalSum" class="hidden">
+			<div id="lFinalSum"><b>Soma final: </b><div class="finalSumValue green inline"></div></div>
+			<div id="l3-remainderFinalSum"><b>Resto por 3 da soma final: </b><div id="3-remainderFinalSumValue" class="green inline"></div></div>
+		</div>
+	</div>
 </div>	
+
+
 
 <div class="spaced-v">
   <button id="showAnswer" type="button" class="btn btn-default btn-success spaced-h" onClick="showAnswer();">Mostrar Explicação</button> 
 </div> 
 
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
+	<script src="assets/jquery-ui/js/jquery-ui.min.js"></script>
         <script type="text/javascript" src="src/js/interactive114.js"></script>
 	
 	<div>	
--- 114-Operacoes-num-tabuleiro-3x3/src/js/interactive114.js
+++ 114-Operacoes-num-tabuleiro-3x3/src/js/interactive114.js
@@ -1,169 +1,223 @@
-var d=1, l=5, h;
 
-var A, B, C;
-var AB, BC, CA;
-var P;
-var qboard;
-var x,y,z;
-var a,b;
-var Q, G, H, I;
-var QG, GH, HI;
 
 var showingSolution;
 
-function generateTriangle(){
-	
-	h=l*Math.sqrt(3)/2;
-	qboard = JXG.JSXGraph.initBoard('questionJXGBox', {boundingbox: [-d, h+d, l+3*d, -d],  keepaspectratio: true, showcopyright: false});
+var i, j;
 
-	//vertices of triangle
-	A = qboard.create('point', [0,0], {name: "A", color:'blue', fixed:true, label:{offset:[-10,-10]}});
-	B = qboard.create('point', [l, 0], {name: "B", color:'blue', fixed:true});
-	C = qboard.create('point', [l/2, h], {name: "C", color:'blue', fixed:true});	
-	
-	//sides of triangle
-	AB = qboard.create('segment', [A, B], {color:'black', fixed:true});
-	BC = qboard.create('segment', [B, C], {color:'black', fixed:true});
-	CA = qboard.create('segment', [C, A], {color:'black', fixed:true});
-	
+var gameTableSum, finalTableSum;
+var actualRemainder, finalRemainder;
+
+
+function generateGameTable(){
+	$("#gameTable").empty();
+	tableInnerHtml="";
+	var elem;
+	gameTableSum=0;
+	for(var i=0; i<5; i++){
+		tableInnerHtml+="<tr>";
+		for(var j=0; j<4; j++){
+			if(i==0 && j==0) tableInnerHtml+="<td class='align-bottom border-none downRightButton'><button type='button' class='btn btn-default'><img src='src/images/arrow_down_right.png'></button></td>";
+			else if(i==4&&j==0) tableInnerHtml+="<td class='center border-none upRightButton'><button type='button' class='btn btn-default'><img src='src/images/arrow_up_right.png'></button></td>";
+			else if(i==0) tableInnerHtml+="<td id='db_"+j+"' class='center border-none downButton'><button type='button' class='btn btn-default'><img src='src/images/arrow_down.png'></button></td>";
+			else if(j==0) tableInnerHtml+="<td id='rb_"+i+"' class='border-none rightButton'><button type='button' class='btn btn-default'><img src='src/images/arrow_right.png'></button></td>";
+			else if(i!=4) {
+				elem = Math.floor(Math.random()*2);
+				tableInnerHtml+="<td id='g_"+i+"_"+j+"' class='center' value='"+elem+"'><b>"+elem+"</b></td>";
+				gameTableSum+= elem;
+			}
+			
+		}
+		tableInnerHtml+="</tr>";
+	}
+	$("#gameTable").html(tableInnerHtml);
+	updateGameTableSum();
 }
 
-function generateRandomInnerPoint(){
-	minPercent = 0.2;
-	maxPercent = 0.8;
-	//random factor
+function colorSquaresOnChange(id){
+	$( id ).animate({
+	  backgroundColor: "#00FFFF",//light-blue
+	}, 1 );//ms
+	$( id ).animate({
+	  backgroundColor: "#FFF",//white
+	}, 400 );//ms
+}
+
+function addDropdownValueOnElem(id, dropdownValue){
+	newValue =  parseInt($(id).attr("value"))+parseInt(dropdownValue);
+	console.log(id+" "+$(id).attr("value"));
+	$(id).attr("value",newValue);
+	$(id).html("<b>"+newValue+"</b>");
+}
+
+function updateGameTableSum(){
+	gameTableSum=0;
+	for(var i=1; i<4; i++){
+		for(var j=1; j<4; j++){
+			gameTableSum+=parseInt($("#g_"+i+"_"+j).attr("value"));
+			console.log('$(#g_'+i+'_'+j+').attr("value")'+ $("#g_"+i+"_"+j).attr("value"));
+			console.log('parseInt '+parseInt($("#g_"+i+"_"+j).attr("value")));
+		}
+	}
+	$(".gameSumValue").html("<b>"+gameTableSum+"</b>");
+	actualRemainder = gameTableSum%3;
+	if(actualRemainder<0) actualRemainder+=3;
+	$(".3-remainderGameSumValue").html("<b>"+actualRemainder+"</b>");
+}
+
+function actionButtons(){
+	var id;
+	//down Right Diagonal
+	$(".downRightButton").click(function(){
+		var dropdownValue = $('#numberToAdd').val();
+		//summing dropdownValue on all elements of downRight Diagonal
+		for(i=1; i<4; i++){
+			_id="_"+i+"_"+i;
+			id="#g"+_id;
+			addDropdownValueOnElem(id, dropdownValue);
+			colorSquaresOnChange(id);
+			checkIfNumberIsCorrect(_id);
+		}    		
+		updateGameTableSum();
+	});
 	
-	do{
-		a = Math.random();
-	}
-	while(a < minPercent||
-		a > maxPercent);
-	do{
-		b = Math.random();
-	}
-	while(b < minPercent||
-		b > maxPercent);
+	//up Right Diagonal
+	$(".upRightButton").click(function(){
+		var dropdownValue = $('#numberToAdd').val();
+		//summing dropdownValue on all elements of upRight Diagonal
+		for(i=1; i<4; i++){
+			_id="_"+i+"_"+(4-i);
+			id="#g"+_id;
+			addDropdownValueOnElem(id, dropdownValue);
+			colorSquaresOnChange(id);
+			checkIfNumberIsCorrect(_id);
+		}
+		updateGameTableSum();    			
+	});
 
-	if(a<=1/2){
-		b=b*h*2*a;
-		a=a*l;
-	}
-	else{
-		b=b*h*2*(1-a);
-		a=a*l;
-	}
-	P = qboard.create('point', [a, b], {name: "P", color:'red', fixed:false, label:{offset:[0, 15]}});
+	//lines
+	$(".rightButton").click(function(){
+		var dropdownValue = $('#numberToAdd').val();
+		coords = $(this).attr("id").split("_");//coords[0]=rb //prefix   //coords[1]=i //line  
+		//summing dropdownValue on all elements of line i
+		for(i=1; i<4; i++){
+			_id="_"+coords[1]+"_"+i;
+			id="#g"+_id;
+			addDropdownValueOnElem(id, dropdownValue);
+			colorSquaresOnChange(id);
+			checkIfNumberIsCorrect(_id);
+		}
+		updateGameTableSum();    		
+	});
 
-	generateProjections();
-
-	P.on("drag", function(){
-		clearElements();
-		generateProjections();
+	//columns
+	$(".downButton").click(function(){
+		var dropdownValue = $('#numberToAdd').val();
+		coords = $(this).attr("id").split("_");//coords[0]=db //prefix   //coords[1]=i //column 
+		//summing dropdownValue on all elements of column j
+		for(i=1; i<4; i++){
+			_id = "_"+i+"_"+coords[1];
+			id="#g"+_id;
+			addDropdownValueOnElem(id, dropdownValue);
+			colorSquaresOnChange(id);
+			checkIfNumberIsCorrect(_id);
+		}
+		updateGameTableSum();   		
 	});
 
 }
 
-function generateProjections(){
-	
-	//projections to sides
-	D = qboard.create('perpendicularpoint', [P, BC], {name:"D", withLabel:true, color:'green'});
-	E = qboard.create('perpendicularpoint', [P, CA], {name:"E", withLabel:true, color:'green', label:{offset:[-15,0]}});
-	F = qboard.create('perpendicularpoint', [P, AB], {name:"F", withLabel:true, color:'green', label:{offset:[0,-15]}});
-
-	//projections of inner rectangle vertices to the sides
-	PD = qboard.create('segment', [P, D], {name:"x", withLabel:true, color:'violet', label:{offset:[0, 10]}});
-	PE = qboard.create('segment', [P, E], {name:"y", withLabel:true, color:'yellow'});
-	PF = qboard.create('segment', [P, F], {name:"z", withLabel:true, color:'cyan'});
-
-	x = distance(P, D);
-	y = distance(P, E);
-	z = distance(P, F);
-
-	if(showingSolution){
-
-		//points of the resulting sum of projection segments.
-		Q = qboard.create('point', [l+2*d, 0], {name:"Q", withLabel:false, fixed:true, color:'blue'});
-		G = qboard.create('point', [l+2*d, z], {name:"G", withLabel:false, fixed:true, color:'cyan'});
-		H = qboard.create('point', [l+2*d, z+y], {name:"H", withLabel:false, fixed:true, color:'yellow'});
-		I = qboard.create('point', [l+2*d, z+y+x], {name:"I", withLabel:false, fixed:true, color:'violet'});
-
-		J = qboard.create('point', [l+2*d, h], {color:'violet', withLabel:false, fixed:true});
-
-		// projection segments.
-		QG = qboard.create('segment', [Q, G], {name:"z", withLabel:true, color:'cyan', label:{offset:[-20, 0]}});
-		GH = qboard.create('segment', [G, H], {name:"y", withLabel:true, color:'yellow', label:{offset:[-20, 0]}});
-		HI = qboard.create('segment', [H, I], {name:"x", withLabel:true, color:'violet', label:{offset:[-20, 0]}});
-		
-		//dashed segments limiting height
-		CJ = qboard.create('segment', [C, J], { color:'black', dash:2});
-		BQ = qboard.create('segment', [B, Q], { color:'black', dash:2});
-	}	
+function checkAllSquares(){
+	for (var i=1; i<4; i++){
+		for (var j=1; j<4; j++){
+			_id="_"+i+"_"+j;
+			checkIfNumberIsCorrect(_id);
+		}
+	}
 }
 
-function distance (a, b){
-	dx = a.X()-b.X();
-	dy = a.Y()-b.Y();
-	return Math.sqrt(dx*dx+dy*dy);
+function generateFinalTable(){
+	$("#desiredFinalTable").empty();
+	tableInnerHtml="";
+	var elem;
+	finalTableSum=0;
+	for(var i=0; i<5; i++){
+		tableInnerHtml+="<tr>";
+		for(var j=0; j<4; j++){
+			if(i===3 && j===3){//last square number of final table
+				if( (finalTableSum - gameTableSum) % 3 === 0) elem=1;
+				else elem=0;
+				//in the end, finalTableSum and gameTableSum need to have different remainder by 3.
+
+				tableInnerHtml+="<td id='f_"+i+"_"+j+"' value='"+elem+"' class='center'><i class='glyphicon glyphicon-ok hidden'></i><b>"+elem+"</b></td>";
+				finalTableSum+=elem;
+			}
+			else if(i>0 && j>0 && i!=4)	{
+				elem = Math.floor(Math.random()*2);
+				tableInnerHtml+="<td id='f_"+i+"_"+j+"' value='"+elem+"' class='center'><i class='glyphicon glyphicon-ok hidden'></i><b>"+elem+"</b></td>";
+				finalTableSum+=elem;
+			}
+			else tableInnerHtml+="<td id='f_"+i+"_"+j+"' class='border-none'></td>";
+		}
+		tableInnerHtml+="</tr>";
+	}
+	$("#desiredFinalTable").html(tableInnerHtml);
+	$(".finalSumValue").html("<b>"+finalTableSum+"</b>");
+	finalRemainder = finalTableSum%3;
+	if(finalRemainder<0) finalRemainder+=3;
+	$("#3-remainderFinalSumValue").html("<b>"+finalRemainder+"</b>");
 }
 
-function clearElements(){
-	D.remove();
-	E.remove();
-	F.remove();
-
-	PD.remove();
-	PE.remove();
-	PF.remove();
-
-	if(showingSolution){
-
-		Q.remove();
-		G.remove();
-		H.remove();
-		I.remove();
-
-		QG.remove();
-		GH.remove();
-		HI.remove();	
-
-		CJ.remove();
-		BQ.remove();
+function checkIfNumberIsCorrect(_id){
+	$i = $("#f"+_id).find("i");
+	if($("#g"+_id).attr("value") === $("#f"+_id).attr("value")){//correct value on square.
+		$("#f"+_id).addClass("bg-green");
+		$i = $("#f"+_id).find("i");
+		$i.removeClass("hidden");
+	}
+	else{
+		$("#f"+_id).removeClass("bg-green");
+		$i = $("#f"+_id).find("i");
+		$i.addClass("hidden");
 	}
 }
 
 function generateSolution(){
 	showingSolution=true;
-	generateProjections();
+	$("#dGameSum").removeClass("hidden");
+	$("#dFinalSum").removeClass("hidden");
+	updateGameTableSum();
 }
 
 function showAnswer(){
 	$("#showAnswer").attr("disabled",true);
 	$("#answerExplanation").removeClass("hidden");
-	$("#answerExplanation").html("<b>Solução:</b><br/>"+
-	"<div class='justify'>Note que:</div>"+
-	"<div class='center'>[ABC] = [PAB] + [PBC] + [PCA]</div>"+
-	"<div class='justify'>Onde [XYZ] é a área do triângulo XYZ.</div>"+
-	"<div class='justify'>Como a área do triangulo é a metade do produto da base pela altura: </div>"+
-	"<div class='center'>`(l*h)/2` = `(l*z)/2` + `(l*x)/2` + `(l*y)/2`</div>"+
-	"<div class='justify'>Logo:</div>"+
-	"<div class='center'>`h = z + x + y`</div>"+
-	"<div class='justify'>Movimente o ponto P, na figura, agora com a solução explícita.</div>");
+	$("#answerExplanation").html("<div class='justify'><b>Solução:</b></div>"+
+	"<div class='justify'>Temos 2 possibilidades: ou realizamos algumas operações tentando chegar ao estado final pedido, utilizando o tabuleiro interativo. Ou tentamos analisar o porquê de não ser possível chegar ao estado final.</div>"+
+	"<div class='justify'>Em problemas desse tipo é comum analisarmos invariantes em suas operações. Isto é, procurar propriedades que não mudam independente das operações realizadas.</div>"+
+	"<div class='justify'>Note que todas as operações realizadas se resumem a somar 3 números +1 em 3 casas ou somar 3 números -1. Uma invariante interessante seria o resto da divisão por 3 da soma dos números de todas as casinhas do tabuleiro. Como a cada operação somamos +3 ou -3 a soma total, o resto da divisão por 3 na soma total permanece inalterado.</div>"+
+	"<div class='justify'>Analisando o valor da soma das casas do tabuleiro:</div>"+
+	"<div class='center'><b>Soma atual do tabuleiro: </b><div class='gameSumValue green inline'></div></div>"+
+	"<div class='center'><b>Soma final: <div class='green inline'>"+finalTableSum+"</div></b></div>"+
+	"<div class='justify'>E analisemos os os restos por 3 das somas:</div>"+
+	"<div class='center'><b>Resto por 3 da soma atual: </b><div class='3-remainderGameSumValue green inline'></div></div>"+
+	"<div class='center'><b>Resto por 3 da soma final: <div class='green inline'>"+finalRemainder+"</div></b></div>"+
+	"<div class='justify'>Como os restos por 3 de ambas as somas são distintos e essa propriedade é invariante com as operações, o estado final do tabuleiro solicitado não pode ser atingido.</div>"+
+	"<div class='justify'> Realize as operações no tabuleiro, agora, com os valores das somas explícitas abaixo dos tabuleiros.</div>");
 	generateSolution();
-	compileMathJaxCode();
 }
 
 function resetAnswer(){
 	showingSolution=false;
 	$("#showAnswer").attr("disabled",false);
 	$("#answerExplanation").addClass("hidden");
+	$("#dGameSum").addClass("hidden");
+	$("#dFinalSum").addClass("hidden");
 }	
-
-function compileMathJaxCode(){
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-}
 
 function generateNewGame(){
 	resetAnswer();
-	generateTriangle();
-	generateRandomInnerPoint()
+	generateGameTable();
+	generateFinalTable();
+	actionButtons();
+	checkAllSquares();
 }
