ea8e76efebc43dd1743eea7383c29e140234ed00
Einstein do Nascimento <einsteinnjr@gmail.com>
2015-01-23 12:46:25 -0300

    [115] Finalized.

--- 115-Soma-de-angulos-notavel/index.php
+++ 115-Soma-de-angulos-notavel/index.php
@@ -5,15 +5,17 @@
 	<link rel="stylesheet" type="text/css" href="assets/jsxgraph/css/jsxgraph.css">
         <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
         <link href="assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" type="text/css" href="src/css/interactive113.css">
+	<link rel="stylesheet" type="text/css" href="src/css/interactive115.css">
 </head>
 <body>
 <div class="spaced-v">
-  <button type="button" class="btn btn-default  btn-primary" onClick="generateNewGame()">Novo ponto</button>
+  <button type="button" class="btn btn-default  btn-primary" onClick="generateNewGame()">Nova configuração</button>
 </div>
 <div class="spaced-v bordered">
 	<div id="questionJXGBox" class="jxgbox center-img"></div>
 </div>	
+<div><b>Fonte do Problema:</b> <a href="http://clubes.obmep.org.br/blog/probleminha-uma-soma-notavel/">Clubes Obmep - Uma soma notável</a>
+</div>
 
 <div class="spaced-v">
   <button id="showAnswer" type="button" class="btn btn-default btn-success spaced-h" onClick="showAnswer();">Mostrar Explicação</button> 
@@ -21,8 +23,7 @@
 
 	<script src="assets/jquery/jquery-1.10.2.min.js"></script>
 	<script src="assets/jsxgraph/js/jsxgraphcore.js"></script>
-        <script type="text/javascript" src="src/js/interactive113.js"></script>
-	<script type="text/javascript" src="assets/MathJax/MathJax.js"></script>
+        <script type="text/javascript" src="src/js/interactive115.js"></script>
 	
 	<div>	
 		<div id="answerExplanation" class="bordered answer-bg hidden height-auto">
@@ -31,12 +32,6 @@
 	<script>
 	$(document).ready(function () {
 		generateNewGame();
-	});
-	MathJax.Hub.Config({
-	  config: ["MMLorHTML.js"],
-	  jax: ["input/AsciiMath","output/HTML-CSS","output/NativeMML"],
-	  extensions: ["asciimath2jax.js","MathMenu.js","MathZoom.js"],
-	  imageFont: null
 	});
 	</script>
 </body>
---
+++ 115-Soma-de-angulos-notavel/src/js/interactive115.js
@@ -0,0 +1,137 @@
+var d=1, l=5, h;
+
+var A, B, C, D;
+var AB, BC, CD, DA;
+var P, Q;
+var qboard;
+var PQ, AQ, DQ, CP, DP;
+var alpha, beta, teta;
+var t1;
+
+
+var showingSolution;
+
+function generateSquare(){
+	delta=0.25;
+
+	x = (delta+ Math.random()*(1-2*delta))*l;  // 0.2l until 0.8l
+
+	qboard = JXG.JSXGraph.initBoard('questionJXGBox', {boundingbox: [-d, l+d, l+d, -d],  keepaspectratio: true, showcopyright: false});
+
+	//vertices of square
+	A = qboard.create('point', [0,0], {name: "A", color:'blue', fixed:true, label:{offset:[-10,-10]}});
+	B = qboard.create('point', [0, l], {name: "B", color:'blue', fixed:true});
+	C = qboard.create('point', [l, l], {name: "C", color:'blue', fixed:true});
+	D = qboard.create('point', [l, 0], {name: "D", color:'blue', fixed:true});
+
+	t1 = qboard.create('text',[0, -d/2, "<span style='text-decoration:overline'>PB</span> + <span style='text-decoration:overline'>BQ</span> = l = <span style='text-decoration:overline'>AB</span>"]);
+
+	//sides of square
+	AB = qboard.create('segment', [A, B], {color:'black', fixed:true});
+	BC = qboard.create('segment', [B, C], {color:'black', fixed:true});
+	CD = qboard.create('segment', [C, D], {color:'black', fixed:true});	
+	DA = qboard.create('segment', [D, A], {color:'black', fixed:true});
+
+	//side points
+	P = qboard.create('point', [0, x], {name: "P", color:'red', label:{offset:[-15, 0]}});
+	Q = qboard.create('point', [function(){return P.Y();}, l], {name: "Q", color:'red'});
+	
+	//p sliding on AB
+	P.makeGlider(AB);
+
+	PQ = qboard.create('segment', [P, Q], {color:'black'});
+	AQ = qboard.create('segment', [A, Q], {color:'black', dash:2});
+	DP = qboard.create('segment', [D, P], {color:'black', dash:2});
+	DQ = qboard.create('segment', [D, Q], {color:'black', dash:2});
+	CP = qboard.create('segment', [C, P], {color:'black', dash:2});
+
+	alpha = qboard.create('angle', [Q,A,P], {name: "&alpha;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:1}),
+        beta = qboard.create('angle', [Q,D,P], {name: "&beta;", color: 'green', type:'sector', orthoType:'square', orthoSensitivity:2, radius:1});
+	teta = qboard.create('angle', [Q,C,P], {name: "&theta;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:1});
+}
+
+
+function clearElements(){
+	D.remove();
+	E.remove();
+	F.remove();
+
+	PD.remove();
+	PE.remove();
+	PF.remove();
+
+	if(showingSolution){
+
+		Q.remove();
+		G.remove();
+		H.remove();
+		I.remove();
+
+		QG.remove();
+		GH.remove();
+		HI.remove();	
+
+		CJ.remove();
+		BQ.remove();
+	}
+}
+
+function generateSolutionInfosOnImage(){
+	
+	//labels
+	AP = qboard.create('segment', [A, P], {name:'y', withLabel:true, color:'black', label:{offset:[-15, 0]}});
+	BP = qboard.create('segment', [B, P], {name:'x', withLabel:true, color:'black', label:{offset:[-15, 0]}});
+	BQ = qboard.create('segment', [B, Q], {name:'y', withLabel:true, color:'black'});
+	CQ = qboard.create('segment', [C, Q], {name:'x', withLabel:true, color:'black'});
+
+	//labels
+	DA.setAttribute({
+		name: 'l',
+    		withLabel: true,
+		label: {offset:[-10,-10]}
+	});
+	CD.setAttribute({
+		name: 'l',
+    		withLabel: true
+	});
+	t1.remove();
+	t1 =qboard.create('text',[0, -d/2, "x + y = <span style='text-decoration:overline'>PB</span> + <span style='text-decoration:overline'>BQ</span> = l = <span style='text-decoration:overline'>AB</span>"]);
+
+	alpha = qboard.create('angle', [P,D,A], {name: "&alpha;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:1}),
+	teta = qboard.create('angle', [C,D,Q], {name: "&theta;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:1});
+}
+
+function generateSolution(){
+	showingSolution=true;
+	generateSolutionInfosOnImage();
+}
+
+function showAnswer(){
+	$("#showAnswer").attr("disabled",true);
+	$("#answerExplanation").removeClass("hidden");
+	$("#answerExplanation").html("<b>Solução:</b><br/>"+
+	"<div class='justify'>Note que os triângulos &Delta;ABQ e &Delta;DAP são congruentes (caso LAL), já que: </div>"+
+	"<div class='center'><span style='text-decoration:overline'>PY</span> = l - x = y = <span style='text-decoration:overline'>BQ</span> e <span style='text-decoration:overline'>AB</span> = l = <span style='text-decoration:overline'>AD</span></div>"+
+	"<div class='justify'>Assim:</div>"+
+	"<div class='center'>&ang;BAQ = &alpha; = &ang;PDA</div>"+	
+	"<div class='justify'>Note também que os triângulos &Delta;BCP e &Delta;CDQ são congruentes (caso LAL), já que: </div>"+
+	"<div class='center'><span style='text-decoration:overline'>QC</span> = l - y = x = <span style='text-decoration:overline'>BP</span> e <span style='text-decoration:overline'>BC</span> = l = <span style='text-decoration:overline'>CD</span></div>"+
+	"<div class='justify'>Assim:</div>"+
+	"<div class='center'>&ang;BCP = &beta; = &ang;CDQ</div>"+
+	"<div class='justify'>Daí:</div>"+
+	"<div class='center'>&ang;PAQ + &ang;PDQ + &ang;PCQ = &alpha; + &beta; + &theta; = &ang;ADP + &ang;PDQ + &ang;QDC = 90<sup>o</dup> </div>");
+	generateSolution();
+}
+
+function resetAnswer(){
+	showingSolution=false;
+	$("#showAnswer").attr("disabled",false);
+	$("#answerExplanation").addClass("hidden");
+}	
+
+function generateNewGame(){
+	//resetAnswer();
+	generateSquare();
+	//generateTriangle();
+	//generateRandomInnerPoint()
+}
