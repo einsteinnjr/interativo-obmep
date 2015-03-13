1a180ceb144a5fafbc05f9db6aabc89332775500
Einstein do Nascimento <einsteinnjr@gmail.com>
2015-03-13 14:11:15 -0300

    [121] finalized.

--- 121-Cevianas-do-triangulo-retangulo/src/js/interactive121.js
+++ 121-Cevianas-do-triangulo-retangulo/src/js/interactive121.js
@@ -1,83 +1,104 @@
-var d = 6; // padding until figure.
+var d = 8; // padding until figure.
 var d1 = 2;// distance until quotes infos
 var x_A, y_A, x_B, y_B, x_P, y_P ;
 var x, y, d;
 
-var MAX_X = 20;
-var MIN_X = MAX_X * 3/4;
-var MAX_Y = 10;
-var MIN_Y = 2;
+var MAX_X, MAX_Y;
 
-var A, B, O, P, C;
+var A, B, O, P;
+var MIN_ANGLE = Math.PI/6; // 30 degrees
+var DELTA_ANGLE = 2*Math.PI/3; // 120 degrees
+var r=10;
 var qboard;
-var t1;
+var t1, t2, t3;
 
+var is1stSolution;
+var detailedAnswer;
 
 var showingSolution;
 
 function generateFigure(){
 
-	qboard = JXG.JSXGraph.initBoard('questionJXGBox', {boundingbox: [-d, MAX_Y+d, MAX_X+d, -(MAX_Y+d)],  keepaspectratio: true, showcopyright: false});
-	
-	//point A = (0,y)
-	y_A = MIN_Y+ Math.floor(Math.random()*(MAX_Y +1 -MIN_Y));  // MIN_Y to MAX_Y, arbitrarily
+	// theta defines where C will be located in the semicircle
+	theta = MIN_ANGLE + Math.random()*DELTA_ANGLE; // 30 < theta < 150
 
-	//point B = (x_B, y_B)
-	x_B = MIN_X + Math.floor(Math.random()*(MAX_X + 1 - MIN_X)); // MIN_X to MAX_X, arbitrarily
-	do{
-		y_B = MIN_Y + Math.floor(Math.random()*(MAX_Y + 1 - MIN_Y)); // MIN_Y to MAX_Y, arbitrarily
-	} while (y_B===y_A);
+	qboard = JXG.JSXGraph.initBoard('questionJXGBox', {boundingbox: [ -(r+d1), r+d1, r+d1, -(d1)],  keepaspectratio: true, showcopyright: false});
 
-	max_AB = Math.max(y_A+d1, y_B+d1);
-
-	//plotting A points
-	A = qboard.create('point', [0, y_A], {name: "A", color:'blue', fixed:true, label:{offset:[-15, 0]}});
-	A1 = qboard.create('point', [-d1, y_A], { color:'black', fixed:true, label:{offset:[-15, 0]}, withLabel:false, face:"+"});
-	A2 = qboard.create('point', [0, max_AB], { color:'black', fixed:true, label:{offset:[-15, 0]}, withLabel:false, face:"+"});
-
-	//plotting B points
-	B = qboard.create('point', [x_B, y_B], {name: "B", color:'blue', fixed:true});
-	B1 = qboard.create('point', [x_B + d1, y_B], {color:'black', fixed:true, label:{offset:[-15, 0]}, withLabel:false, face:"+"});
-	B2 = qboard.create('point', [x_B, max_AB], { color:'black', fixed:true, withLabel:false, face:"+"});
-
-	//plotting origin O
+	// center of circle
 	O = qboard.create('point', [0, 0], {name: "O", color:'blue', fixed:true, visible:false});
-	O1 = qboard.create('point', [-d1, 0], {name: "O", color:'blue', fixed:true, visible:false});
 
-	//plotting Q, projection of B on x
-	Q = qboard.create('point', [x_B, 0], {name: "Q", color:'blue', fixed:true, visible:false});
-	Q1 = qboard.create('point', [x_B+d1, 0], {name: "Q", color:'blue', fixed:true, visible:false});
-	
-	//movable point
-	P = qboard.create('point', [x_B/2, 0], {name: "P", color:'red', label:{offset:[-15,-15]}, needsRegularUpdate:true});
+	A = qboard.create('point', [-r, 0], {name: "A", color:'blue', fixed:true, label:{offset:[-15, 0]}});
+	B = qboard.create('point', [r, 0], {name: "B", color:'blue', fixed:true, label:{offset:[10, 0]}});
 
-	//path
-	AP = qboard.create('segment', [A, P], {color:'green'});
-	BP = qboard.create('segment', [B, P], {color:'green'});
-	
-	x = dist(O, A);
-	y = dist(Q, B);
-	d = dist(A2, B2);
+	arc = qboard.create('arc', [O, B, A], {strokeColor:'black'});
 
-	// heigths distances: to show distance values
-	O1A1 = qboard.create('segment', [O1, A1], {name:"x="+x, color:'black', dash:2, withLabel:true, label:{offset:[-30, 0]}});
-	B1Q1 = qboard.create('segment', [B1, Q1], {name:"y="+y, color:'black', dash:2, withLabel:true});
-	A2B2 = qboard.create('segment', [A2, B2], {name:"d="+d, color:'black', dash:2, withLabel:true});
-	
-	r = qboard.create('line', [O, Q], {name: "r", color:'black', fixed:true, withLabel:true, label:{offset:[10, -15]}});
+	console.log("theta "+theta+" ou "+theta*180/Math.PI);
 
-	//p needs to be on r
-	P.makeGlider(r);
+	C = qboard.create('point', [r*Math.cos(theta), r*Math.sin(theta)], {name: "C", color:'red', label:{offset:[-15, 10]}});
 
-	t1 = qboard.create('text',[-d1, -(MAX_Y+d1), "<span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span> = "+(dist(P,A)+dist(P,B)).toFixed(2)], {fixed:true});
-	t2 = qboard.create('text',[-d1, -(MAX_Y+2*d1), "min(<span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span>) = ?"], {fixed:true});
-	
-	//when P moves along r, recalculate PA+PB
-	P.on("drag", function(){
-		t1.remove();
-		t1 = qboard.create('text',[-d1, -(MAX_Y+d1), "<span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span> = "+(dist(P,A)+dist(P,B)).toFixed(2)], {fixed:true});
+	// make C only walk on the semicircle
+	C.makeGlider(arc);
+
+	//sides
+	CA = qboard.create('segment', [C, A], {color:'black', strokeWidth:1});
+	CB = qboard.create('segment', [C, B], {color:'black', strokeWidth:1});
+	AB = qboard.create('segment', [A, B], {color:'black', strokeWidth:1});
+
+	//height
+	D = qboard.create('perpendicularpoint', [C, AB], {color:'blue', strokeWidth:1});
+	CDA = qboard.create('angle', [C, D, A], {withLabel:false, color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:0.7});
+
+	//midpoint
+	M = qboard.create('midpoint', [A, B], {name:"M", color:'blue', strokeWidth:1});
+
+	//midarc
+	BMC = qboard.create('angle', [B, M, C], {strokeColor:'black', visible:false});
+
+	//bisector
+	bisector = qboard.create('bisector', [A, C, B], {color:'red', strokeWidth:1, visible:false});
+	E = qboard.create('intersection', [bisector, AB], {color:'blue', strokeWidth:1});
+
+	//cevian points
+	//height
+	CD = qboard.create('segment', [C, D], {color:'green', strokeWidth:1});
+	//median
+	CM = qboard.create('segment', [C, M], {color:'green', strokeWidth:1});
+	//bisector
+	CE = qboard.create('segment', [C, E], {color:'red', strokeWidth:1});
+		
+	//angles
+	plotDesiredAnglesAndInfos();
+	C.on("drag", function(){
+		if(!showingSolution){
+			cleanAnglesAndInfos();
+			plotDesiredAnglesAndInfos();
+		}
+		else{
+			cleanSolutionAnglesAndInfos();
+			plotSolutionAnglesAndInfos();
+		}
 	});
+}
 
+function plotDesiredAnglesAndInfos(){
+	if(BMC.Value()*180/Math.PI < 90){ //C on 1st quadrant
+		ECD = qboard.create('angle', [E, C, D], {withLabel:false, color: 'green', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		ECM = qboard.create('angle', [M, C, E], {withLabel:false, color: 'green', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2.5});
+	}
+	else{ //C on 2nd quadrant
+		ECD = qboard.create('angle', [D, C, E], {withLabel:false, color: 'green', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		ECM = qboard.create('angle', [E, C, M], {withLabel:false, color: 'green', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2.5});
+	}
+	t1 = qboard.create('text',[-r, -d1, "&ang;ECD = "+(ECD.Value()*180.0/Math.PI).toFixed(2)], {fixed:true});
+
+	t2 = qboard.create('text',[-r, -2*d1, "&ang;ECM = "+(ECD.Value()*180.0/Math.PI).toFixed(2)], {fixed:true});//some rounding error. Changed to equal ECD value.
+}
+
+function cleanAnglesAndInfos(){
+	ECD.remove();
+	ECM.remove();
+	t1.remove();
+	t2.remove();
 }
 
 function dist(A, B){
@@ -86,60 +107,71 @@ function dist(A, B){
 	return Math.sqrt(dx*dx + dy*dy);
 }
 
-function generateSolutionInfosOnImage(){
+function cleanSolutionAnglesAndInfos(){
+	//use X to not rely on which side it is.
+	CXD.remove();
+	DCX.remove();
+	XCM.remove();
+	MXC.remove();
+}
 
-	C = qboard.create('reflection', [B, r], {name: "B'", color:'blue', fixed:true, label:{offset:[15,-15]}});
-	BC = qboard.create('segment', [B, C], {color:'blue', fixed:true, dash:2});
+function plotSolutionAnglesAndInfos(){
 
-	M = qboard.create('midpoint', [B, C], {name: "M", color:'blue', fixed:true, dash:2, label:{offset:[15,-15]}});
+	if(BMC.Value()*180/Math.PI < 90){//C on 1st quadrant
+		CXD = qboard.create('angle', [C, B, D], {name:"&beta;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		DCX = qboard.create('angle', [D, C, B], {name:"&alpha;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		XCM = qboard.create('angle', [A, C, M], {name:"&alpha;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		MXC = qboard.create('angle', [M, A, C], {name:"&alpha;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
 
-	x_N = B.X()*A.Y()/(A.Y()+B.Y());
-	
-	N = qboard.create('point', [x_N, 0], {name: "N", color:'yellow', label:{offset:[-15,-15]}, fixed:true});
-
-	// solution path
-	AN = qboard.create('segment', [A, N], {color:'yellow'});
-	BN = qboard.create('segment', [B, N], {color:'yellow'});
-	CN = qboard.create('segment', [C, N], {color:'yellow', dash:2});
-
-	CP = qboard.create('segment', [C, P], {color:'green', dash:2});
-	
-	t2.remove();
-	t2 = qboard.create('text',[-d1, -(MAX_Y+2*d1), "min(<span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span>) = <span style='text-decoration:overline'>NA</span> + <span style='text-decoration:overline'>NB</span> ="+(dist(A,C).toFixed(2))], {fixed:true});
+	}
+	else{//C on 2nd quadrant
+		CXD = qboard.create('angle', [D, A, C], {name:"&alpha;", color: 'pink', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		DCX = qboard.create('angle', [A, C, D], {name:"&beta;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		XCM = qboard.create('angle', [M, C, B], {name:"&beta;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+		MXC = qboard.create('angle', [C, B, M], {name:"&beta;", color: 'blue', type:'sector', orthoType:'square', orthoSensitivity:2, radius:2});
+	}
 }
 
 function generateSolution(){
 	showingSolution=true;
-	generateSolutionInfosOnImage();
+	plotSolutionAnglesAndInfos();
 }
 
 function showAnswer(){
+	cleanAnglesAndInfos();
+	generateSolution();
 	$("#showAnswer").attr("disabled",true);
 	$("#answerExplanation").removeClass("hidden");
 	$("#answerExplanation").html("<b>Solução:</b><br/>"+
-	"<div class='justify'>Às vezes, uma construção geométrica simplifica o problema. A condição de 'tocar na reta r' pode ser reformulada:</div>"+
-	"<div class='justify'>Analisemos o ponto B', reflexo de B em relação a reta r, tal que <span style='text-decoration:overline'>MB</span> = <span style='text-decoration:overline'>MB'</span> e <span style='text-decoration:overline'>BB'</span> &perp; r.</div> "+
-	"<div class='justify'>O ponto B' é tal que a soma das distâncias de P a A e B é a mesma que a A e B', para todo ponto P da reta r, isto é:</div>"+
-	"<div class='center'><span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span> = <span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB'</span> <span class='tab'>(I)</span> </div>"+
-	"<div class='justify'>Isso ocorre porque &Delta;PMB &cong; &Delta;PMB' (Caso LAL).</div>"+
-	"<div class='center'><span style='text-decoration:overline'>PM</span> comum;<span class='tab'></span>  &ang;PMB = 90<sup>o</sup>=&ang;PMB' <span class='tab'></span>e <span style='text-decoration:overline'>MB</span> = <span style='text-decoration:overline'>MB'</span>;</div>"+
-	"<div class='justify'>Com B' e A estão de lados opostos da reta r, chamemos de N a interseção de <span style='text-decoration:overline'>AB'</span> e a reta r.</div>"+
-	"<div class='justify'>Para qualquer ponto P na reta R, pela desigualdade triangular no &Delta;PAB':</div>"+
-	"<div class='center'><span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB'</span> &ge; <span style='text-decoration:overline'>AB'</span> = <span style='text-decoration:overline'>AN</span> + <span style='text-decoration:overline'>NB'</span></div>"+
-	"<div class='justify'>Aplicando (I) para os pontos P e N:</div>"+
-	"<div class='center'><span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB</span>  = <span style='text-decoration:overline'>PA</span> + <span style='text-decoration:overline'>PB'</span> &ge; <span style='text-decoration:overline'>AB'</span> = <span style='text-decoration:overline'>AN</span> + <span style='text-decoration:overline'>NB'</span> = <span style='text-decoration:overline'>NA</span> + <span style='text-decoration:overline'>NB</span> </div>"+
-	"<div class='justify'>Assim, N é o ponto procurado.</div>"+
-	"<div class='justify'>Note que para calcular a soma desejada <span style='text-decoration:overline'>AN</span> + <span style='text-decoration:overline'>NB'</span> basta acharmos o valor de <span style='text-decoration:overline'>AB'</span>, que é hipotenusa de um triângulo retângulo de catetos d e (x+y), então:</div>"+
-	"<div class='center'><span style='text-decoration:overline'>AN</span> + <span style='text-decoration:overline'>NB'</span> = <span style='text-decoration:overline'>AB'</span> = &radic;<span style='text-decoration:overline'>h</span><sup>2</sup><span style='text-decoration:overline'>+(x+y)</span><sup>2</sup> = "+Math.sqrt(d*d+(x+y)*(x+y)).toFixed(2)+"</div>");
-	generateSolution();
+	"<div class='justify'>O &Delta;ABC é retângulo em C, assim, temos: "+
+	"<div class='center'>&ang;CAB = &alpha; ,<span class='tab'/> &ang;CBA = &beta; <span class='tab'/> e <span class='tab'/>&alpha; + &beta; = 90&deg;</div>"+
+	"<div class='justify'>Como CD é altura do &Delta;ACB:</div>"+
+	"<div class='center'>&Delta;CDB ~ &Delta;ACB &rArr; &ang;DCB = &alpha;</div>"+
+	"<div class='center'>&Delta;ADC ~ &Delta;ACB &rArr; &ang;DCA = &beta;</div>"+
+	"<div class='justify'>Como &ang;ACB é reto, <span style='text-decoration:overline'>AB</span> é diâmetro do circuncirculo de &Delta;ABC. Como M é ponto médio de <span style='text-decoration:overline'>AB</span> e, portanto, de um diâmetro do circuncículo, M é o centro da circunferência circunscrita a &Delta;ABC. Assim:</div>"+
+	"<div class='center'><span style='text-decoration:overline'>MA</span> = <span style='text-decoration:overline'>MB</span> = <span style='text-decoration:overline'>MC</span></div>"+
+	"<div class='justify'>Logo:</div>"+
+	"<div class='center'>&Delta;AMC é isósceles em M &rArr; &ang;CAM = &ang; ACM = &alpha;  </div>"+
+	"<div class='center'>&Delta;BMC é isósceles em M &rArr; &ang;CBM = &ang; BCM = &beta;  </div>"+
+	"<div class='justify'>Como CE é bissetriz do &ang;ACB:</div>"+
+	"<div class='center'>&ang;ACE = &ang;BCE = 45&deg;</div>"+
+	"<div class='justify'>Supondo &alpha; < &beta; (C no 1&deg; quadrante. O outro caso é análogo):</div>"+
+	"<div class='center'>&ang;ECM = &ang;ECA - &ang;MCA = 45&deg; - &alpha; </div>"+
+	"<div class='center'>&ang;ECD = &ang;ECB - &ang;DCB = 45&deg; - &alpha; </div>"+
+	"<div class='justify'>Assim:</div>"+
+	"<div class='center'>&ang;ECM = &ang;ECD </div>"+
+	"<div class='justify'>Um resultado mais geral: Escolhido um vértice de um triângulo qualquer, a reta que o liga ao circuncentro é isogonal a altura relativa a esse vértice. (2 retas são 'isogonais' se passam pelo mesmo vértice e são simétricas em relação à bissetriz do ângulo interno a esse vértice). Você consegue provar? </div>"+
+	"<div class='justify'>Nesse interativo, como o ponto médio M da hipotenusa é o circuncentro, estamos analisando um caso particular aplicado a triângulos retângulos.");
+	
 }
 
 function resetAnswer(){
 	showingSolution=false;
 	$("#showAnswer").attr("disabled",false);
 	$("#answerExplanation").addClass("hidden");
-}	
+}		
 
 function generateNewGame(){
 	generateFigure();
+	resetAnswer();
 }
